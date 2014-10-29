cdcb5afceb8f0a1f06c318dc6df3df52d1d8f72d
Lioman <dev@lioman.de>
2014-10-29 21:03:07 +0100

    smitching machine

--- puphpet/files/startup-always/0-startDjangoServer.sh
+++ puphpet/files/startup-always/0-startDjangoServer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "starting development server"
-cd /var/www/gipmunk
-python3 manage.py runserver 0.0.0.0:8000&
+cd /var/www/django/notes_project
+python manage.py runserver 0.0.0.0:8000&
 
